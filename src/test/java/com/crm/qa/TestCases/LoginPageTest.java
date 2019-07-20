6f958b0adc9bed4957663d38cffd4af3ee3a222c
pavankrishnan77 <pavankrishnan1993@gmail.com>
2019-07-20 14:21:17 +0530

    Modified

--- src/test/java/com/crm/qa/TestCases/LoginPageTest.java
+++ src/test/java/com/crm/qa/TestCases/LoginPageTest.java
@@ -3,6 +3,7 @@ package com.crm.qa.TestCases;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.crm.qa.BaseClass.TestBase;
