4f703336d3f3f2d1a077ee200c34836d8f31c75b
Pavan Kumar K J <pavankrishnan1993@gmail.com>
2020-07-11 10:46:40 +0530

    Updated Home Page Title

--- src/main/java/com/crm/qa/Constants/Constants.java
+++ src/main/java/com/crm/qa/Constants/Constants.java
@@ -15,7 +15,7 @@ public class Constants
 	public static final int LONG_WAIT = 10000;
 	
 	public static final String LOGIN_PAGE_TITLE = "CRMPRO - CRM software for customer relationship management, sales, and support.";
-	public static final String HOME_PAGE_TITLE = "CRMPRO123";
+	public static final String HOME_PAGE_TITLE = "CRMPRO";
 
 	public static final String TEST_DATA_SHEET_PATH = System.getProperty("user.dir") + "/src/main/java/com/crm/qa/TestData/FreeCRMTestData.xlsx";
 }
